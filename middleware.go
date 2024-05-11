6820f29e19d29873e3082ad81ce535d03e236d66
DESKT-DMCDGMMAS\Administrator <995116474@qq.com>
2024-05-11 16:48:31 +0800

    添加打印gin参数的中间件

--- middleware.go
+++ middleware.go
@@ -1,8 +1,11 @@
 package loggerx
 
 import (
+	"bytes"
 	"context"
+	"io"
 
+	"github.com/gin-gonic/gin"
 	uuid "github.com/satori/go.uuid"
 )
 
@@ -11,4 +14,35 @@ import (
 // 自动设置请求ID
 func SetTradeId(ctx context.Context) context.Context {
 	return context.WithValue(ctx, "trade_id", uuid.NewV4().String())
+}
+
+// 中间件 gin框架保存请求相关信息
+func MiddlewareGetGinParams(log *Logger) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		buf := &bytes.Buffer{}
+		tea := io.TeeReader(ctx.Request.Body, buf)
+		body, err := io.ReadAll(tea)
+		if err != nil {
+			// panic(err)
+		}
+
+		// 截取body的前1000个字符
+		bodys := string(body)
+		if len(bodys) > 1000 {
+            bodys = bodys[:1000]
+        }
+
+		ctx.Request.Body = io.NopCloser(buf)
+
+		m := map[string]interface{}{
+			"method": ctx.Request.Method,
+			"uri":    ctx.Request.RequestURI,
+			"body":   bodys,
+			"query":  ctx.Request.URL.Query(),
+			"header": ctx.Request.Header,
+		}
+		log.Infof(ctx, "request %+v", m)
+		ctx.Next()
+
+	}
 }
